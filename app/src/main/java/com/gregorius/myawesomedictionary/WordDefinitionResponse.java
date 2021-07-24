85213f2b437a41f76deab9984faac9494a6c9d21
Gregorius Albert <galbert.djaja@gmail.com>
2021-07-24 18:58:23 +0700

    Add alias on model

--- app/src/main/java/com/gregorius/myawesomedictionary/WordDefinitionResponse.java
+++ app/src/main/java/com/gregorius/myawesomedictionary/WordDefinitionResponse.java
@@ -1,8 +1,20 @@
 package com.gregorius.myawesomedictionary;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class WordDefinitionResponse {
+
+    @SerializedName("img_url")
+    @Expose
     public String img_url;
+
+    @SerializedName("type")
+    @Expose
     public String type;
+
+    @SerializedName("definition")
+    @Expose
     public String definition;
 
 }
